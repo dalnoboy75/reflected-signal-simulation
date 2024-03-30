54176a3536056ceedeca2c7092d3bdf7130db995
Ivan Novikov <novikovivan005@gmail.com>
2024-03-30 13:36:04 +0300

    gui and object movement modulation

--- backend/object.py
+++ backend/object.py
@@ -12,10 +12,12 @@ class Object:
     def change_velocity(self, new_velocity) -> None:
         self.velocity = new_velocity
 
-    def get_location_at_time(self, time):
-        if time == 0:
-            return self.coordinate
-        return self.get_location_at_time(time - 1) + self.velocity
+    def get_location_at_time(self, time) -> list:
+        coords = list()
+        coords.append(self.coordinate)
+        for i in range(1, time + 1):
+            coords.append(coords[i - 1] + self.velocity)
+        return coords
 
     def get_location(self) -> Point:
         return self.coordinate
--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,18 @@
 from PyQt6 import QtWidgets
+from gui import Ui_MainWindow
 
-
-class Window(QtWidgets.QMainWindow):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication([])
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec())
+
+

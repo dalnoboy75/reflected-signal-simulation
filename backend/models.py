696351d546e50301e255c9661f5652a1646bf092
Ivan Novikov <novikovivan005@gmail.com>
2024-04-13 00:00:42 +0300

    distance and speed information

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 class Point:
     def __init__(self, x: float, y: float, z: float) -> None:
         self.x = x
@@ -5,15 +8,29 @@ class Point:
         self.z = z
 
     def __add__(self, other):
-        return Point(self.x+other.x, self.y+other.y, self.z + other.z)
+        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+
     def __mul__(self, other):
-        return Point(self.x*other, self.y*other, self.z*other)
+        return Point(self.x * other, self.y * other, self.z * other)
 
     def __rmul__(self, other):
         return self.__mul__(other)
 
     def __str__(self):
         return f"{self.x} {self.y} {self.z}"
+
     def __repr__(self):
         return self.__str__()
 
+    def __abs__(self):
+        return sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+    def __neg__(self):
+        p = Point(-self.x, -self.y, -self.z)
+        return p
+
+    def __sub__(self, other):
+        return self.__add__(other.__neg__())
+
+    def __rsub__(self, other):
+        return self.__sub__(other)
--- frontend/app.py
+++ frontend/app.py
@@ -2,6 +2,8 @@ from PyQt6 import QtWidgets
 from gui import Ui_MainWindow
 from backend.object import Object
 from backend.models import Point
+
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -9,21 +11,25 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.ui.simulatePushButton.clicked.connect(lambda :self.simulate())
+        self.ui.simulatePushButton.clicked.connect(lambda: self.simulate())
 
     def simulate(self):
-        coords = Point(self.ui.coords_x.value(),self.ui.coords_y.value(),self.ui.coords_z.value())
-        speed = Point(self.ui.speed_x.value(), self.ui.speed_y.value(), self.ui.speed_z.value(),)
-        object = Object(coords, speed)
-        positions = object.get_location_at_time(10)
+        coords = Point(self.ui.coords_x.value(), self.ui.coords_y.value(), self.ui.coords_z.value())
+        speed = Point(self.ui.speed_x.value(), self.ui.speed_y.value(), self.ui.speed_z.value())
+        station_coords = Point(self.ui.station_coords_x.value(), self.ui.station_coords_y.value(), self.ui.station_coords_z.value())
+        print(coords, speed, station_coords)
+        self.ui.distance_label.setText(f'{abs(coords - station_coords):.3f}')
+        self.ui.speed_label.setText(f'{abs(speed):.3f}')
+        obj = Object(coords, speed)
+        positions = obj.get_location_at_time(10)
         for i in positions:
             print(i)
 
+
 if __name__ == '__main__':
     import sys
+
     app = QtWidgets.QApplication([])
     mainWindow = MainWindow()
     mainWindow.show()
     sys.exit(app.exec())
-
-

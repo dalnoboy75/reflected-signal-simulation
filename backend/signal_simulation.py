9492ed1384ee80fa6ac71598034166de18c158b3
Ivan Novikov <novikovivan005@gmail.com>
2024-05-13 20:02:17 +0300

    fix

--- backend/signal_simulation.py
+++ backend/signal_simulation.py
@@ -5,25 +5,18 @@ from backend.rls import RLS
 
 
 def simulate(data: dict) -> dict:
-    print(1)
     rl = RLS(coordinates=np.array(list(data["RLS"]["COORD"])), amplification_coefficient=data["RLS"]["AMPL"],
-             fix_coefficient=1, energy=data["RLS"]["ENERGY"], impulse_count=5)
+             fix_coefficient=1, energy=data["RLS"]["ENERGY"], impulse_count=data["IMPC"])
     obj = Entity(coordinates=np.array(list(data["OBJ"]["COORD"])), radius=data["OBJ"]["RADIUS"],
                  velocity=np.array(list(data["OBJ"]["SPEED"])))
     muf = Muffler(noise_share=data["DISTORTION"])
-    print(2)
     rl.receiver.get_signal(obj, rl.radiator.power, rl.radiator.wave_length)
 
     dt = data["DT"]
-    print(3)
     muffled_distance = rl.calculate_distance(obj, muf)
-    print(4)
     dir_to_obj_vector = rl.receiver.get_direction(obj)
-    print(5)
     obj_coords = rl.calculate_coordinate(obj, dir_to_obj_vector, muf)[0]
-    print(6)
     speed_vector = rl.calculate_velocity(obj, dir_to_obj_vector, muf, dt)
-    print(7)
     speed = np.linalg.norm(speed_vector)
     res = {"MUFDIST": muffled_distance, "SPEED": speed, "SIGMA": obj.reflection_surface,
            "WAVEL": rl.radiator.wave_length, "L": 1, "OBJCOORD": obj_coords}
--- frontend/app.py
+++ frontend/app.py
@@ -28,20 +28,31 @@ class MainWindow(QtWidgets.QMainWindow):
                     "SPEED": (self.ui.obj_speed_x.value(), self.ui.obj_speed_y.value(), self.ui.obj_speed_z.value()),
                     "RADIUS": self.ui.obj_radius.value()}, "RLS": {"COORD": (
                 self.ui.station_coords_x.value(), self.ui.station_coords_y.value(), self.ui.station_coords_z.value()),
-                "DIRECTION": (self.ui.station_direction_x.value(), self.ui.station_direction_y.value(),
-                              self.ui.station_direction_z.value()), "ENERGY": self.ui.station_energy.value(),
-                "CRENERGY": self.ui.station_cr_energy.value(), "AMPL": self.ui.station_amplification.value()},
-            "DISTORTION": self.ui.station_distortion.value(), "DT": self.ui.station_delta.value()}
-        res = simulate(self.data)
+                "ENERGY": self.ui.station_energy.value(),
+                "AMPL": self.ui.station_amplification.value()},
+            "DISTORTION": self.ui.station_distortion.value() / 100, "DT": self.ui.station_delta.value(),"INPC":self.ui.impulse_count.value()}
+        try:
+            res = simulate(self.data)
+        except:
+            QtWidgets.QMessageBox.critical(self, 'Error', 'Something went wrong. Please, put different values.',
+                                           buttons=QtWidgets.QMessageBox.StandardButton.Ok,
+                                           defaultButton=QtWidgets.QMessageBox.StandardButton.Ok)
+            return
+        self.results = res
         self.distance = res["MUFDIST"]
         self.speed = res["SPEED"]
         self.sigma = res["SIGMA"]
         self.wave_len = res["WAVEL"]
         self.fc = res["L"]
         self.coords = res["OBJCOORD"]
-        print(4)
-        self.ui.distance_label.setText(str(self.distance))
-        self.ui.speed_label.setText(str(self.speed))
+        print(self.distance, self.speed)
+        if np.isnan(self.distance) or np.isnan(self.speed):
+            QtWidgets.QMessageBox.critical(self, 'Error', 'Something went wrong. Please, put different values.',
+                                           buttons=QtWidgets.QMessageBox.StandardButton.Ok,
+                                           defaultButton=QtWidgets.QMessageBox.StandardButton.Ok)
+        else:
+            self.ui.distance_label.setText(f'{self.distance:.3f}')
+            self.ui.speed_label.setText(f'{self.speed:.3f}')
         '''
         TODO:
         считываем данные (DONE)
@@ -72,27 +83,28 @@ class MainWindow(QtWidgets.QMainWindow):
                                            defaultButton=QtWidgets.QMessageBox.StandardButton.Ok)
             return
 
+        print(1)
         self.scene.clear()
         self.scene.set_size_inches(5, 5)
         self.obj_coords = self.data["OBJ"]["COORD"]
-
+        print(2)
         axes = self.scene.add_subplot((0, 0.05, 1, 0.9), projection='3d', facecolor='lightblue')
-
+        print(3)
         axes.cla()
 
         dist_rl_obj = np.array(list(self.obj_coords)) - np.array(list(self.data["RLS"]["COORD"]))
 
-        real_dist = np.linalg.norm(dist_rl_obj)
-
+        real_dist = self.results["MUFDIST"]
+        print(4)
         axes.plot([self.data["RLS"]["COORD"][0], self.obj_coords[0]],
                   [self.data["RLS"]["COORD"][1], self.obj_coords[1]],
                   [self.data["RLS"]["COORD"][2], self.obj_coords[2]])
 
         list_center = [self.obj_coords, self.data["RLS"]["COORD"], self.data["RLS"]["COORD"]]
         list_radius = [self.data["OBJ"]["RADIUS"], 1, real_dist]
-        list_color = [('red', 0.8), ('green', 0.8), ('blue', 0.3)]
+        list_color = [('red', 0.8), ('green', 0.8), ('blue', 0.1)]
         names = ["OBJECT", "RLS", "DISTANCE"]
-
+        print(5)
         min_, max_ = float("inf"), 0
         for name_, c, r, draw in zip(names, list_center, list_radius,
                                      list_color):
@@ -101,20 +113,23 @@ class MainWindow(QtWidgets.QMainWindow):
             x = r * np.cos(u) * np.sin(v)
             y = r * np.sin(u) * np.sin(v)
             z = r * np.cos(v)
+            print(6)
             min_ = min(np.amin(x), np.amin(y), np.amin(z),
                        min_)  # lowest number in the array
             max_ = max(np.amax(x), np.amax(y), np.amax(z),
                        max_)  # highest number in the array
             axes.plot_surface(c[0] - x, c[1] - y, c[2] - z, color=draw[0],
                               alpha=draw[1], label=name_)
+            print(7)
 
         axes.text(
             self.data["RLS"]["COORD"][0], self.data["RLS"]["COORD"][1],
             self.data["RLS"]["COORD"][2],
-            f"dist={round(real_dist, 3)}", size=15,
-            zorder=5, zdir=(dist_rl_obj / np.linalg.norm(dist_rl_obj)),
+            f"distance={round(float(real_dist), 3)}", size=15,
+            zorder=5, zdir=(list(dist_rl_obj / np.linalg.norm(dist_rl_obj))),
             color="darkorange"
         )
+        print(8)
         axes.set_xlim3d(min_, max_)
         axes.set_ylim3d(min_, max_)
         axes.set_zlim3d(min_, max_)
